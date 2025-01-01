821daa182664042bfb4812c7d2fd336beffbc77b
KaloyanStankov11 <kaloqn.t@mail.bg>
2025-01-01 18:59:20 +0200

    Initial commit

---
+++ src/main/java/com/example/documentsregister/DocumentsRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.documentsregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentsRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentsRegisterApplication.class, args);
+    }
+
+}
